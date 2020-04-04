56fc8cb0fdb2825cddaeaca3d84000ae1e9831a9
Prasanna12510 <prasanna@hooq.tv>
2020-04-05 03:15:53 +0800

    trigger infra-deploy pipeline

--- student_service/infra-deploy/monitoring.tf
+++ student_service/infra-deploy/monitoring.tf
@@ -1,1 +1,1 @@
-############### This file only for the ALARMS and ALERTS that does not contain any autoscaling actions#####
+################ This file only for the ALARMS and ALERTS that does not contain any autoscaling actions#####
