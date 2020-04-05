40b7b1223b49a31148c664be62da564c72136bea
Prasanna12510 <prasanna@hooq.tv>
2020-04-06 05:25:26 +0800

    fix syntax issue

--- student_service/infra-deploy/locals.tf
+++ student_service/infra-deploy/locals.tf
@@ -26,7 +26,7 @@ locals {
       "backend_protocol"                 = "HTTP"
       "backend_port"                     = 80
       "slow_start"                       = 0
-      "backend_path"                     = "/actuator/health
+      "backend_path"                     = "/actuator/health"
       "target_type"                      = "instance"
     }
   ]
